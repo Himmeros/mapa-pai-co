20918b478ae2c94697e12ebdd066471ed41fdcb2
Proyectos Himmeros <midiam1@yahoo.com>
2026-07-12 13:45:40 -0400

    Update mapa-pai-co.php

--- mapa-pai-co.php
+++ mapa-pai-co.php
@@ -3,7 +3,7 @@
  * Plugin Name: Mapa PAI Co.
  * Plugin URI:  https://github.com/Himmeros/mapa-pai-co/
  * Description: Sistema de directorio y gestión de clientes mediante atajos, creado para Páginas Amarillas en Internet.
- * Version:     1.1.1
+ * Version:     1.1.2
  * Author:      Proyectos Himmeros
  * Text Domain: himmeros.xyz
  */
