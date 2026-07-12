0b901e6e65e5a15729138f2164822a5a6fdc0029
Proyectos Himmeros <midiam1@yahoo.com>
2026-07-12 15:47:10 -0400

    +Detalles
    Un detalle con las actualizaciones

--- includes/plugin-info.php
+++ includes/plugin-info.php
@@ -21,7 +21,7 @@ function mapa_pai_info_plugin( $result, $action, $args ) {
     }
 
     $plugin_info->sections['description'] = '<h3>Mapa PAI Co.</h3><p>Directorio interactivo de ubicaciones y clientes diseñado a la medida, gestionado desde GitHub.</p>';
-    $plugin_info->sections['changelog']   = '<h4>v1.1.3</h4><ul><li>Ajustes de conexión con la API de actualizaciones.</li></ul>';
+    $plugin_info->sections['changelog']   = '<h4>v1.1.4</h4><ul><li>Ajustes de conexión con la API de actualizaciones.</li></ul>';
 
     return $plugin_info;
 }
